94a9cfb71bd6a4fd96e6b80058d1613c73639c1b
Alex <49230683+Stea11er@users.noreply.github.com>
2021-02-28 06:41:13 +0300

    2

--- lib/main.dart
+++ lib/main.dart
@@ -88,7 +88,10 @@ class _MyHomePageState extends State<MyHomePage> {
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => BoardPage()));
+        },
         child: Text("Enter",
             textAlign: TextAlign.center,
             style: style.copyWith(
@@ -159,5 +162,149 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     ));
+  }
+}
+
+class NavigationBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final blockNavigation = new Container(
+      decoration: new BoxDecoration(
+        color: new Color(0xFFFFFFFF),
+        shape: BoxShape.rectangle,
+        borderRadius: new BorderRadius.circular(8.0),
+        boxShadow: <BoxShadow>[
+          new BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10.0,
+            offset: new Offset(0.0, 10.0),
+          ),
+        ],
+      ),
+    );
+
+    final buttonHome = new MaterialButton(
+      onPressed: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => BoardPage()));
+      },
+      child: new Image(
+        image: new AssetImage("assets/home.png"),
+        height: 50.0,
+        width: 50.0,
+      ),
+    );
+
+    final buttonPush = new MaterialButton(
+      onPressed: () {
+        //Navigator.push(
+        //context, MaterialPageRoute(builder: (context) => BoardPage()));
+      },
+      child: new Image(
+        image: new AssetImage("assets/push.png"),
+        height: 50.0,
+        width: 50.0,
+      ),
+    );
+
+    final buttonGraph = new MaterialButton(
+      onPressed: () {
+        //Navigator.push(
+        //context, MaterialPageRoute(builder: (context) => BoardPage()));
+      },
+      child: new Image(
+        image: new AssetImage("assets/graph.png"),
+        height: 50.0,
+        width: 50.0,
+      ),
+    );
+
+    final buttonAuth = new MaterialButton(
+      onPressed: () {
+        //Navigator.push(
+        //context, MaterialPageRoute(builder: (context) => BoardPage()));
+      },
+      child: new Image(
+        image: new AssetImage("assets/auth.png"),
+        height: 50.0,
+        width: 50.0,
+      ),
+    );
+
+    return new Container(
+        height: 50.0,
+        margin: const EdgeInsets.symmetric(
+          vertical: 16.0,
+          horizontal: 24.0,
+        ),
+        child: new Stack(
+          children: <Widget>[
+            blockNavigation,
+            Row(
+              children: [buttonHome, buttonPush, buttonGraph, buttonAuth],
+            ),
+          ],
+        ));
+  }
+}
+
+class PlanetRow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final cloudThumbnail = new Container(
+      alignment: FractionalOffset.centerLeft,
+      child: new Image(
+        image: new AssetImage("assets/cloud.png"),
+        height: 92.0,
+        width: 92.0,
+      ),
+    );
+
+    final bourdCard = new Container(
+      decoration: new BoxDecoration(
+        color: new Color(0xFFFFFFFF),
+        shape: BoxShape.rectangle,
+        borderRadius: new BorderRadius.circular(8.0),
+        boxShadow: <BoxShadow>[
+          new BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10.0,
+            offset: new Offset(0.0, 10.0),
+          ),
+        ],
+      ),
+    );
+
+    return new Container(
+        height: 448.0,
+        margin: const EdgeInsets.symmetric(
+          vertical: 16.0,
+          horizontal: 24.0,
+        ),
+        child: new Stack(
+          children: <Widget>[
+            bourdCard,
+            cloudThumbnail,
+          ],
+        ));
+  }
+}
+
+class BoardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xFFD2D2D2),
+        body: Stack(
+          children: [
+            new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[new PlanetRow()]),
+            new Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [new NavigationBar()],
+            )
+          ],
+        ));
   }
 }
